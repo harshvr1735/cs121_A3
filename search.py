fda4717ed72ae72e0601b9472ccc20c5e6220924
leeat5 <156141910+leeat5@users.noreply.github.com>
2025-03-02 16:20:58 -0800

    implemented use of positions file for faster lookup

--- search.py
+++ search.py
@@ -3,13 +3,46 @@ import json
 import re
 import time
 import nltk.tokenize
-import math
 from nltk.stem import PorterStemmer
 import numpy as np
+import math
 
 complete_index_directory = os.path.join(os.getcwd(), "complete_index")
 stemmer = PorterStemmer()
 tokenizer = nltk.tokenize.RegexpTokenizer(r'[a-zA-Z0-9]+')
+
+def read_json(file_path, position, key):
+    file_path.seek(position)
+    chunk = file_path.read(1000) ## may need to up the threshold for v v common words ?
+
+    try:
+        start = chunk.find('{"word":')
+        end = chunk.find('}', start) + 1
+        word_postings = chunk[start:end]
+
+        print("ACTUAL CHUNK: ", word_postings)
+        print()
+        data = json.loads(word_postings)
+        if data.get("word") == key:
+            return data.get("postings", [])
+        
+    except json.JSONDecodeError:
+        pass
+
+    print("NOT FOUND")
+    return []
+
+def load_positions():
+    positions_file = os.path.join(complete_index_directory, "positions.json")
+    with open(positions_file, "r") as f:
+        positions = json.load(f)
+    
+    position_map = {}
+    for entry in positions:
+        prefix = entry["prefix"]
+        position_map[prefix] = entry["position"]
+    
+    return position_map
 
 def load_inverted_index():
     index = {}
@@ -22,125 +55,153 @@ def load_inverted_index():
             index[name_split] = (open(path, "r"))
     return index
 
-#Cosine Similarity Calculation
 
-#Computing the dot product of two vectors
+
+
+
+####
 def dot_product(vector1, vector2):
     return sum(v1 * v2 for v1, v2 in zip(vector1, vector2))
 
-#Computing the magnitude of a vector
 def vector_magnitude(vector):
     return math.sqrt(sum(v ** 2 for v in vector))
 
-#Computing the cosine similarity between two vectors
 def cosine_similarity(vector1, vector2):
     vectors_product = dot_product(vector1, vector2)
     vector1_magnitude = vector_magnitude(vector1)
     vector2_magnitude = vector_magnitude(vector2)
+    print("vector magnitudes", vector1_magnitude, vector2_magnitude)
+    print("vectors product",vectors_product)
     if vector1_magnitude == 0 or vector2_magnitude == 0:
         return 0
-    return vectors_product / (vector1_magnitude * vector2_magnitude)
+    similarity = vectors_product / (vector1_magnitude * vector2_magnitude)
+    print("SIMILARTIY: ",similarity)
+    print()
+    return similarity
 
-#Converting query into a vector of TF-IDF scores
 def get_query_vector(query_terms, index):
     query_vector = []
     for term in query_terms:
         term = stemmer.stem(term)
         try:
+            prefix = prefix_getter(term)
+
             file = index[prefix]
             file.seek(0)
             a = json.load(file)
-            if term in a:
-                query_vector.append(a[term][0][1])
+            # print("query vector: ", a)
+            for entry in a:
+                # print("entry: ", entry)
+                if term == entry["word"]:
+                    print("IF TERM IN A")
+                    # print(entry['postings'][0][1])
+                    query_vector.extend([posting[1] for posting in entry["postings"]])
+                    break
             else:
                 query_vector.append(0)
         except KeyError:
             query_vector.append(0)
+    print(query_vector)
     return query_vector
 
-#Converting document into a vector of TF-IDF scores for query terms
 def get_document_vector(doc_id, query_terms, index):
     doc_vector = []
     for term in query_terms:
         term = stemmer.stem(term)
-        prefix = prefix_getter(index, term)
+        prefix = prefix_getter(term)
         try:
             file = index[prefix]
             file.seek(0)
             a = json.load(file)
-            if term in a:
-                tf_idf_score = next((item[1] for item in a[term] if item[0] == doc_id), 0)
-                doc_vector.append(tf_idf_score)
+            for entry in a:
+                # print("entry: ", entry)
+                if term == entry["word"]:
+            # if term in a:
+            
+                    tf_idf_score = next((posting[1] for posting in entry["postings"] if posting[0] == doc_id), 0)
+                    print("DOCUMENT VECTOR TDIDF: ", tf_idf_score)
+                    doc_vector.append(tf_idf_score)
             else:
                 doc_vector.append(0)
         except KeyError:
             doc_vector.append(0)
     return doc_vector
+#####
 
-def index_getter(index, input):
+
+
+
+
+
+def index_getter(index, positions, input, docID_url):
     start = time.time()
+    # split = tokenizer.tokenize(input)
+    # split = [stemmer.stem(token.lower()) for token in tokenizer.tokenize(input)]
     query_terms = tokenizer.tokenize(input)
-    query_terms = [stemmer.stem(term) for term in query_terms]
+    query_terms = [stemmer.stem(term.lower()) for term in query_terms]
     query_vector = get_query_vector(query_terms, index)
     # print(stems)
     # split = [condition.strip() for condition in input.split("AND")]
-    print(query_terms)
 
     results = []
     for term in query_terms:
-        prefix = prefix_getter(index, term)
         try:
-            file = index[prefix]
-            file.seek(0)
-            a = json.load(file)
-            if term in a:
-                doc_ids = set(item[0] for item in a[term])
-                results.append(doc_ids)
+            prefix = prefix_getter(term)
+            if prefix == "invalid":
+                run(index, docID_url)
+            if not prefix or prefix not in index or prefix not in positions:
+                continue
+            lookupstart = time.time()
+
+            term_position = positions[prefix]
+            if term not in term_position:
+                print(f"{term} not found in index.")
+                continue
+            position = term_position[term]
+            # positions = os.path.join(complete_index_directory, "positions.json")
+
+
+            # path = os.path.join(complete_index_directory, f"complete_index_{prefix}.json")
+            postings = set(doc_id for doc_id, _, _ in read_json(index[prefix], position, term))
+
+            results.append(postings)
+            print("lookup time: ", time.time() -  lookupstart)
         except KeyError:
             print("Not found in index.")
 
-    if not results:
-        print("No results found for the query.")
-        return
-    
-    results.sort(key=len)
-    smallest = results[0] ## more efficient to AND smallest -> mid -> largest
-    for res in results:
-        smallest &= res ## actually TA says & doesnt work that well and returns bad results from a1
-        ## need to do something else
+    if results:
 
-    if smallest:
-        with open("docID_url_map.json", "r") as f:
-            docID_url = json.load(f)
-            doc_scores = []
-            for doc_id in smallest:
-                doc_vector = get_document_vector(doc_id, query_terms, index)
-                similarity = cosine_similarity(query_vector, doc_vector)
-                doc_scores.append((doc_id, similarity))
-            doc_scores.sort(key=lambda x: x[1], reverse=True)
-            for doc_id, score in doc_scores[:10]:
-                print(f"{doc_id}: {docID_url[str(doc_id)]} (Score: {score:.4f})")
+        results.sort(key=len)
+        smallest = results[0] ## more efficient to AND smallest -> mid -> largest
+        for res in results:
+            smallest &= res
+            # smallest = smallest.intersection(res) ## actually TA says & doesnt work that well and returns bad results from a1
+            ## need to do something else
+
+        if smallest:
+            with open("docID_url_map.json", "r") as f:
+                docID_url = json.load(f)
+                doc_scores = []
+                for doc_id in smallest:
+                    doc_vector = get_document_vector(doc_id, query_terms, index)
+                    similarity = cosine_similarity(query_vector, doc_vector)
+                    doc_scores.append((doc_id, similarity))
+                doc_scores.sort(key=lambda x: x[1], reverse=True)
+                for doc_id, score in doc_scores[:10]:
+                    print(f"{doc_id}: {docID_url[str(doc_id)]} (Score: {score:.4f})")
+
 
     end = time.time()
-    print(f"Query took {end - start} seconds to return results.")
-    run(index)
+    print("Query took: ", end - start)
+    run(index, positions, docID_url)
 
-def prefix_getter(index, word):
+def prefix_getter(word):  # names the files and checks prefixes
     if re.match(r'^[0-9]+$', word[0]):
         return "numbers"
-    if word[0] in "abcdef":
-        return "af"
-    elif word[0] in "ghijk":
-        return "gk"
-    elif word[0] in "lmnop":
-        return "lp"
-    elif word[0] in "qrstu":
-        return "qu"
-    elif word[0] in "vwxyz":
-        return "vz"
+    elif word[0] in "abcdefghijklmnopqrstuvwxyz":
+        return word[0]
     else:
-        print("invalid input") ## maybe change ?
-        run(index)
+        return "invalid"
 
 def take_input():
     input1 = input("Input or q to quit: ")
@@ -148,13 +209,16 @@ def take_input():
         exit()
     return input1
 
-def run(index):
+def run(index, positions, docID_url):
     input = take_input()
-    index_getter(index, input)
+    index_getter(index, positions, input, docID_url)
 
 def main():
     index = load_inverted_index()
-    run(index)
+    positions = load_positions()
+    with open("docID_url_map.json", "r") as f:
+        docID_url = json.load(f)
+    run(index, positions, docID_url)
 
 
 if __name__ == "__main__":
