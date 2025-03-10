2a69d07e030909e4f0b8e556df04d1414f258a4c
Arnav Nigam <72648437+ArnavNigam@users.noreply.github.com>
2025-03-09 17:33:49 -0700

    updated to use n-gram and position indexing

--- searcher2.py
+++ searcher2.py
@@ -10,22 +10,27 @@ import time
 # nltk.download('stopwords') ## uncomment if you havent downloaded this before
 # nltk.download('punkt') ## ^^
 
-term_cache = {}
-MAX_CACHE_SIZE = 100
 complete_index_directory = os.path.join(os.getcwd(), "complete_index")
+ngram_index_directory = os.path.join(os.getcwd(), "ngram_index")
+position_index_directory = os.path.join(os.getcwd(), "position_index")
 stemmer = PorterStemmer()
 tokenizer = nltk.tokenize.RegexpTokenizer(r'[a-zA-Z0-9]+')
 stopWords = set(stopwords.words('english'))
 weights = {
     't': 1.5,
-    'a' : 1.4,
+    'a': 1.4,
     'h1': 1.3,
     'h2': 1.2,
     'b': 1.1,
-    'n': 1.0
+    'n': 1.0,
+    '2gram': 1.2,
+    '3gram': 1.3
 }
+term_cache = {}
+MAX_CACHE_SIZE = 100
 
 def read_json(file_path, position, key):
+    """Optimized JSON reader for index files"""
     file_path.seek(position)
     
     chunk_size = 10000 if key not in stopWords else 100000
@@ -89,8 +94,12 @@ def read_json(file_path, position, key):
             
     return []
 
-def load_positions():
-    positions_file = os.path.join(complete_index_directory, "positions.txt")
+def load_positions(directory=complete_index_directory, filename="positions.txt"):
+    """Load position mapping file"""
+    positions_file = os.path.join(directory, filename)
+    if not os.path.exists(positions_file):
+        return {}
+        
     with open(positions_file, "r") as f:
         positions = json.load(f)
     
@@ -101,20 +110,26 @@ def load_positions():
     
     return position_map
 
-def load_inverted_index():
+def load_inverted_index(directory=complete_index_directory, file_prefix="complete_index"):
+    """Load inverted index files"""
     index = {}
-    for file in os.listdir(complete_index_directory):
-        if file.endswith(".txt"):
+    if not os.path.exists(directory):
+        return index
+        
+    for file in os.listdir(directory):
+        if file.startswith(file_prefix) and file.endswith(".txt"):
             name_split = file.split("_")[-1].split(".")[0]
-            path = os.path.join(complete_index_directory, file)
+            path = os.path.join(directory, file)
             index[name_split] = (open(path, "r"))
     return index
 
-def get_cached_postings(index, positions, term):
-    if term in term_cache:
-        return term_cache[term]
+def get_cached_postings(index, positions, term, prefix_getter_func, index_type="standard"):
+    """Get postings with caching for better performance"""
+    cache_key = f"{index_type}_{term}"
+    if cache_key in term_cache:
+        return term_cache[cache_key]
         
-    prefix = prefix_getter(term)
+    prefix = prefix_getter_func(term)
     try:
         term_position = positions[prefix]
         if term not in term_position:
@@ -124,23 +139,64 @@ def get_cached_postings(index, positions, term):
         postings = read_json(index[prefix], position, term)
         
         if len(term_cache) < MAX_CACHE_SIZE:
-            term_cache[term] = postings
+            term_cache[cache_key] = postings
             
         return postings
     except KeyError:
         return []
 
+def get_word_positions(word):
+    """Get word position information from the position index"""
+    positions = load_positions(position_index_directory, "word_positions.txt")
+    position_index = load_inverted_index(position_index_directory, "position_index")
+    
+    prefix = prefix_getter(word)
+    try:
+        term_position = positions[prefix]
+        if word not in term_position:
+            return {}
+            
+        position = term_position[word]
+        postings = read_json(position_index[prefix], position, word)
+        
+        result = {}
+        for posting in postings:
+            doc_id = posting[0]
+            positions_list = posting[1]
+            result[doc_id] = positions_list
+            
+        return result
+    except KeyError:
+        return {}
 
-## COSINE SIM
-def dot_product(vector1, vector2):
-    return np.dot(vector1, vector2)
-
-def vector_magnitude(vector):
-    return np.linalg.norm(vector)
+def proximity_search(term1, term2, max_distance=5):
+    """Find documents where term1 and term2 appear within max_distance words of each other"""
+    positions1 = get_word_positions(term1)
+    positions2 = get_word_positions(term2)
+    
+    result_docs = set()
+    
+    common_docs = set(positions1.keys()) & set(positions2.keys())
+    
+    for doc_id in common_docs:
+        pos1 = [p[1] for p in positions1[doc_id]]
+        pos2 = [p[1] for p in positions2[doc_id]]
+        
+        for p1 in pos1:
+            for p2 in pos2:
+                if abs(p1 - p2) <= max_distance:
+                    result_docs.add(doc_id)
+                    break
+            if doc_id in result_docs:
+                break
+    
+    return result_docs
 
 def cosine_similarity(vector1, vector2):
+    """Calculate cosine similarity between two vectors"""
     v1 = np.array(vector1, dtype=np.float32)
     v2 = np.array(vector2, dtype=np.float32)
+    
     dot = np.dot(v1, v2)
     norm1 = np.linalg.norm(v1)
     norm2 = np.linalg.norm(v2)
@@ -151,12 +207,13 @@ def cosine_similarity(vector1, vector2):
     return dot / (norm1 * norm2)
 
 def get_query_vector(query_terms, index, positions):
+    """Get vector representation of query and term postings"""
     term_postings = {}
     query_vector = []
     cached_postings = {}
     
     for term in query_terms:
-        postings = get_cached_postings(index, positions, term)
+        postings = get_cached_postings(index, positions, term, prefix_getter)
         if not postings:
             query_vector.append(0)
             continue
@@ -170,9 +227,66 @@ def get_query_vector(query_terms, index, positions):
             
     return query_vector, term_postings, cached_postings
 
-def raw_tfidf_ranking(query_terms, term_postings, result_docs, query_vector):
+def get_ngram_postings(query_terms, ngram_index, ngram_positions):
+    """Get n-gram postings for the query terms"""
+    ngram_postings = {}
+    
+    if len(query_terms) >= 2:
+        for i in range(len(query_terms) - 1):
+            bigram = f"{query_terms[i]}_{query_terms[i+1]}"
+            postings = get_cached_postings(ngram_index, ngram_positions, bigram, prefix_getter, "ngram")
+            if postings:
+                ngram_postings[bigram] = set((doc_id, tfidf, gram_type) for doc_id, tfidf, gram_type in postings)
+    
+    if len(query_terms) >= 3:
+        for i in range(len(query_terms) - 2):
+            trigram = f"{query_terms[i]}_{query_terms[i+1]}_{query_terms[i+2]}"
+            postings = get_cached_postings(ngram_index, ngram_positions, trigram, prefix_getter, "ngram")
+            if postings:
+                ngram_postings[trigram] = set((doc_id, tfidf, gram_type) for doc_id, tfidf, gram_type in postings)
+    
+    return ngram_postings
+
+def get_documents_containing_all_terms(query_terms, term_postings):
+    """Find documents containing all query terms"""
+    if not query_terms or not any(term in term_postings for term in query_terms):
+        return set()
+        
+    valid_terms = [term for term in query_terms if term in term_postings]
+    if not valid_terms:
+        return set()
+        
+    sorted_terms = sorted(valid_terms, key=lambda term: len(term_postings.get(term, [])))
+    
+    result_docs = set(doc_id for doc_id, _, _ in term_postings.get(sorted_terms[0], []))
+    
+    if not result_docs:
+        return set()
+        
+    for term in sorted_terms[1:]:
+        term_docs = set(doc_id for doc_id, _, _ in term_postings.get(term, []))
+        result_docs &= term_docs
+        if not result_docs:
+            break
+            
+    return result_docs
+
+def pad_vectors(query_vector, doc_vector):
+    """Ensure vectors are of the same length for similarity calculation"""
+    max_len = max(len(query_vector), len(doc_vector))
+    
+    if len(query_vector) < max_len:
+        query_vector = query_vector + [0] * (max_len - len(query_vector))
+    if len(doc_vector) < max_len:
+        doc_vector = doc_vector + [0] * (max_len - len(doc_vector))
+    
+    return query_vector, doc_vector
+
+def raw_tfidf_ranking(query_terms, term_postings, ngram_postings, result_docs, query_vector):
+    """Rank documents based on TF-IDF and n-grams"""
     doc_scores = {}
     results = []
+    
     batch_size = 500
     result_docs_list = list(result_docs)
     
@@ -193,6 +307,18 @@ def raw_tfidf_ranking(query_terms, term_postings, result_docs, query_vector):
                 else:
                     batch_scores[doc_id].append(weighted_tfidf)
         
+        for ngram, postings in ngram_postings.items():
+            for doc_id, tfidf, gram_type in postings:
+                if doc_id not in batch_docs:
+                    continue
+                
+                weighted_tfidf = tfidf * weights.get(gram_type, 1.2)
+                
+                if doc_id not in batch_scores:
+                    batch_scores[doc_id] = [weighted_tfidf]
+                else:
+                    batch_scores[doc_id].append(weighted_tfidf)
+        
         for doc_id, doc_vector in batch_scores.items():
             padded_query_vector, padded_doc_vector = pad_vectors(query_vector, doc_vector)
             sim = cosine_similarity(padded_query_vector, padded_doc_vector)
@@ -201,54 +327,49 @@ def raw_tfidf_ranking(query_terms, term_postings, result_docs, query_vector):
     results.sort(key=lambda x: x[1], reverse=True)
     return results
 
-def get_documents_containing_all_terms(query_terms, term_postings):
-    if not query_terms or not any(term in term_postings for term in query_terms):
-        return set()
-        
-    valid_terms = [term for term in query_terms if term in term_postings]
-    if not valid_terms:
-        return set()
-        
-    sorted_terms = sorted(valid_terms, key=lambda term: len(term_postings.get(term, [])))
-    result_docs = set(doc_id for doc_id, _, _ in term_postings.get(sorted_terms[0], []))
-    
-    if not result_docs:
-        return set()
-    
-    for term in sorted_terms[1:]:
-        term_docs = set(doc_id for doc_id, _, _ in term_postings.get(term, []))
-        result_docs &= term_docs
-        if not result_docs:
-            break
-            
-    return result_docs
-
-def pad_vectors(query_vector, doc_vector):
-    max_len = max(len(query_vector), len(doc_vector))
-    if len(query_vector) < max_len:
-        query_vector = query_vector + [0] * (max_len - len(query_vector))
-    if len(doc_vector) < max_len:
-        doc_vector = doc_vector + [0] * (max_len - len(doc_vector))
-    return query_vector, doc_vector
-
 def prefix_getter(word):
+    """Determine the prefix for a token"""
+    if not word:
+        return "nonalpha"
+        
     if re.match(r'^[0-9]+$', word[0]):
         return word[0]
     elif word[0] in "abcdefghijklmnopqrstuvwxyz":
         return word[0]
     else:
-        return "invalid"
+        return "nonalpha"
 
 def search_query(query, index, positions, docID_url):
+    """Main search function that returns ranked results and timing information"""
     start_time = time.time()
+    ngram_index = load_inverted_index(ngram_index_directory, "complete_index")
+    ngram_positions = load_positions(ngram_index_directory)
     query_terms = tokenizer.tokenize(query)
     query_terms = [stemmer.stem(term.lower()) for term in query_terms]
     
     if not query_terms:
-        return [], 0
+        return [], time.time() - start_time
     
     query_vector, term_postings, cached_postings = get_query_vector(query_terms, index, positions)
+    
+    ngram_postings = {}
+    if os.path.exists(ngram_index_directory) and ngram_index:
+        ngram_postings = get_ngram_postings(query_terms, ngram_index, ngram_positions)
+    
+    proximity_docs = set()
+    if len(query_terms) > 1 and os.path.exists(position_index_directory):
+        for i in range(len(query_terms) - 1):
+            proxim_results = proximity_search(query_terms[i], query_terms[i+1])
+            if proxim_results:
+                if not proximity_docs:
+                    proximity_docs = proxim_results
+                else:
+                    proximity_docs = proximity_docs.union(proxim_results)
+    
     result_docs = get_documents_containing_all_terms(query_terms, term_postings)
+    
+    if not result_docs and proximity_docs:
+        result_docs = proximity_docs
     
     if not result_docs:
         return [], time.time() - start_time
@@ -257,10 +378,11 @@ def search_query(query, index, positions, docID_url):
     if len(result_docs) > max_docs:
         result_docs = set(sorted(list(result_docs))[:max_docs])
     
-    if len(query_terms) == 1:
+    if len(query_terms) == 1 and not ngram_postings:
         term = query_terms[0]
-        postings = get_cached_postings(index, positions, term)
+        postings = get_cached_postings(index, positions, term, prefix_getter)
         doc_scores = []
+        
         for id, tfidf, style in postings:
             if id in result_docs:
                 weighted_tfidf = tfidf * weights.get(style, 1.0)
@@ -268,7 +390,7 @@ def search_query(query, index, positions, docID_url):
         
         doc_scores.sort(key=lambda x: x[1], reverse=True)
     else:
-        doc_scores = raw_tfidf_ranking(query_terms, term_postings, result_docs, query_vector)
+        doc_scores = raw_tfidf_ranking(query_terms, term_postings, ngram_postings, result_docs, query_vector)
     
     query_time = time.time() - start_time
     return doc_scores, query_time
